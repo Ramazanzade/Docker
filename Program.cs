27d5d3b6a4bdbaab90d270377645d07fab660647
Nizam <ramazanov570633@gmail.com>
2024-11-10 20:56:27 +0400

    Fist

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloWorld;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        for( int i =0 ; i<10000 ;i++)
+        {
+            Console.WriteLine($"Salam Nizam - {i}");
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+    }
+}
